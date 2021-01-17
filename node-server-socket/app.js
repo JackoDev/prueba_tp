2c2a894cf80029bb18374685bfb2c0025c3fd0fb
JackoDev <untaljacko@gmail.com>
2021-01-17 10:45:28 -0500

    [mod] initial config of sockets

--- node-server-socket/app.js
+++ node-server-socket/app.js
@@ -6,10 +6,20 @@ app.get('/', (req, res) => {
   res.send('Conectado al servidor')
 });
 
+// Config de los sockets
+
+io.on('connection', (socket) => {
+  console.log("Nuevo socket conectado");
+
+  socket.on('Realizar peticion de serial', () => {
+    console.log("Realizando Peticion");
+    io.sockes.emit('')
+  })
+});
 
 http.listen(5000, () => {
   console.log("Server listening on port 5000")
-})
+});
 
 
 module.exports = app;
